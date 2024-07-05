4e0ccdf5af0b60d6b374fdb25a91b55afe411bcf
Ueber-James <ueberjames@gmail.com>
2024-07-05 15:49:29 -0300

    env

--- blogpessoal/src/services/Service.ts
+++ blogpessoal/src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal-jfgb.onrender.com/'
+  baseURL: import.meta.env.VITE_API_URl
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
